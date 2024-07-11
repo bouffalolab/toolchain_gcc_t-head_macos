96613abb9f01fb36d7c4b6e1d7fe5d3b50bf2ff7
ybwang <ybwang@bouffalolab.com>
2024-07-11 14:25:03 +0800

    Thead v2.6.1

---
+++ lib/gcc/riscv64-unknown-elf/10.2.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/Volumes/ssd/ybwang/xuantie-gnu-toolchain-2-6-1/riscv-gcc/configure --target=riscv64-unknown-elf CXXFLAGS='-g -O2 ' --disable-libgcctf --prefix=/Volumes/ssd/riscv --disable-shared --enable-threads=posix --enable-languages=c,c++ --with-system-zlib --enable-tls --with-newlib --with-sysroot=/Volumes/ssd/riscv/riscv64-unknown-elf --with-native-system-header-dir=/include --disable-libmudflap --disable-libssp --disable-libquadmath --disable-libgomp --disable-nls --disable-tm-clone-registry --src=.././riscv-gcc --enable-multilib --with-abi=lp64d --with-arch=rv64gcxthead 'CFLAGS_FOR_TARGET=-Os   -mcmodel=medany' 'CXXFLAGS_FOR_TARGET=-Os   -mcmodel=medany'";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "abi", "lp64d" }, { "arch", "rv64gcxthead" } };
